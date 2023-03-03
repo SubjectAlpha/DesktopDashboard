4aee28a5f32e0db515798578b01b4f68aef75ef7
Jake Starr <jakestarr60@gmail.com>
2023-03-02 22:16:34 -0500

    Start forming ui

---
+++ DesktopDashboard/Data/Helpers/ServiceResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace DesktopDashboard.Data.Helpers
+{
+	public class ServiceResponse
+	{
+		public object DataObject;
+		public string Message;
+		public bool Success;
+
+		public ServiceResponse(bool success, string message = "", object o = null)
+		{
+			this.Message = message;
+			this.Success = success;
+			this.DataObject = o;
+		}
+	}
+}
+
---
+++ DesktopDashboard/Data/Services/FileService.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using DesktopDashboard.Data.Helpers;
+
+namespace DesktopDashboard.Data.Services
+{
+	public class FileService
+	{
+		private static string logDir = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "log");
+
+        public FileService()
+		{
+		}
+
+		public static ServiceResponse WriteLog(params string[] messages)
+		{
+			return WriteLogAsync(messages).Result;
+		}
+
+		public static async Task<ServiceResponse> WriteLogAsync(params string[] messages)
+		{
+			var logPath = Path.Combine(logDir, $"[{DateTime.Now.ToShortDateString}]log.txt");
+			var response = new ServiceResponse(false);
+			try
+			{
+				if(!Directory.Exists(logDir))
+				{
+					Directory.CreateDirectory(logDir);
+				}
+
+				if(!File.Exists(logPath))
+				{
+					File.Create(logPath);
+				}
+
+				using var logStream = new FileStream(logPath, FileMode.Append, FileAccess.Write);
+				using var logWriter = new StreamWriter(logStream);
+
+				foreach(var message in messages)
+				{
+					await logWriter.WriteLineAsync(message);
+				}
+
+				response.Success = true;
+			}
+			catch (Exception ex)
+			{
+				response.Message = ex.Message;
+			}
+
+			return response;
+		}
+	}
+}
+
